701a39b8ef84cb3c305ba118fd264e3cefb174c0
nournasser06 <nour.nasser.aaa3.younis@gmail.com>
2026-03-03 23:05:17 +0200

    update: monster class attributes and methods

--- src/game/engine/monsters/Monster.java
+++ src/game/engine/monsters/Monster.java
@@ -1,5 +1,78 @@
 package game.engine.monsters;
-
-public class Monster {
-
+import game.engine.Role;
+public abstract class Monster implements Comparable<Monster>{
+	private String name;
+	private String description;
+	private  Role role;
+	private Role originalRole;
+	private int energy;
+	private  int position;
+	private  boolean frozen;
+	private boolean shielded;
+	private  int confusionTurns;
+	public  Monster(String name, String description, Role originalRole, int energy){
+		this.name = name;
+		this.description = description;
+		this.role = originalRole;
+		this.originalRole = originalRole;
+		this.energy = energy;
+		this.position = 0;
+		this.frozen = false;
+		this.shielded = false;
+		this.confusionTurns = 0;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	public int getEnergy() {
+		return energy;
+	}
+	public void setEnergy(int energy) {
+		
+		this.energy = energy;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	public boolean isFrozen() {
+		return frozen;
+	}
+	public void setFrozen(boolean frozen) {
+		this.frozen = frozen;
+	}
+	public boolean isShielded() {
+		return shielded;
+	}
+	public void setShielded(boolean shielded) {
+		this.shielded = shielded;
+	}
+	public int getConfusionTurns() {
+		return confusionTurns;
+	}
+	public void setConfusionTurns(int confusionTurns) {
+		this.confusionTurns = confusionTurns;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public Role getOriginalRole() {
+		return originalRole;
+	}
+	@Override
+	public int compareTo(Monster o){
+		return this.position - o.position;
+	}
+	
+	
+	
+	
 }
