35517e988616acafc4549f7dfb34e9e659d21b93
salmaabdelbaki0622 <salmaabdelbaki06@gmail.com>
2026-03-05 01:07:56 +0200

    Data updated

--- src/game/engine/dataloader/DataLoader.java
+++ src/game/engine/dataloader/DataLoader.java
@@ -1,8 +1,17 @@
 package game.engine.dataloader;
 import java.io.*;
+
+import game.engine.Role;
 import game.engine.cards.*;
+
 import java.util.ArrayList;
+
+
+
+
+
 import game.engine.cells.*;
+import game.engine.monsters.Monster;
 
 public class DataLoader {
  public static final String CARDS_FILE_NAME ="cards.csv";
@@ -50,6 +59,52 @@ public class DataLoader {
     return cards;
 }
  public static ArrayList<Cell> readCells() throws IOException{
-	 ArrayList<Card> cells= new ArrayList<>();
- }
+	 ArrayList<Cell> cells= new ArrayList<>();
+	 try(BufferedReader br= new BufferedReader(new FileReader(CELLS_FILE_NAME))){
+	    	
+	    	String line;
+	    	while((line=br.readLine())!=null){
+	    		String[] values= line.split(",");
+	    		if(values.length==3){
+	    			String name = values[0];       
+		             Role role = Role.valueOf(values[1]);
+		             int energy = Integer.parseInt(values[2]);
+		             cells.add(new DoorCell(name,role,energy));
+	    		}
+	    		else if(values.length==2){
+	    			String name=values[0];
+	    			int effect=Integer.parseInt(values[1]);
+	    			if(effect<0){
+	    				 cells.add(new ContaminationSock(name,effect));
+	    			}
+	    			else if(effect>0){
+	    				cells.add(new ConveyorBelt(name,effect));
+	    			}
+	    			else{
+	    				throw new IllegalArgumentException("Invalid effect " + effect);
+
+	    			}
+	    		}
+	    		 
+	             }
+	return cells;    	
+}
+	 
+}
+ public static ArrayList<Monster> readCells() throws IOException{
+	 ArrayList<Monster> monsters= new ArrayList<>();
+	 try(BufferedReader br= new BufferedReader(new FileReader(MONSTERS_FILE_NAME))){
+		 String line;
+	    	while((line=br.readLine())!=null){
+	    		String[] values= line.split(",");
+	    		String monsterType=values[0];
+	    		String name=values[1];
+	    		String description=values[2];
+	    		Role role= Role.valueOf(values[3]);
+	    		int energy = Integer.parseInt(values[4]);
+	    	}
+	    	
+	    	
+}
+}
 }
