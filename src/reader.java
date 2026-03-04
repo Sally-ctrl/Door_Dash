02b0d2a837bc0dba203768a0cbe38db5c2414c9d
Sarah Abdelkhalek <sallyahmed571@gmail.com>
2026-03-04 20:50:49 +0200

    update: csv files added and csv test file is also added

---
+++ src/reader.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class reader {
+
+    public static void main(String[] args) {
+
+        String file ="C:\\Users\\sarah\\OneDrive\\Desktop\\cs4\\monsters.csv";
+        BufferedReader reader = null;
+        String line ="";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+
+            while((line = reader.readLine()) != null){
+                String [] row = line.split(",");
+
+                for(String index : row){
+                    System.out.printf(index+"    ");
+                }
+                System.out.println();
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+
+        } finally {
+            try{
+                if(reader != null)
+                    reader.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
