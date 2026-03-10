fd42052694e14025f8295f2b738061fe3ccddabd
salmaabdelbaki0622 <salmaabdelbaki06@gmail.com>
2026-03-10 20:53:58 +0200

    update exception class and interface

--- src/game/engine/exceptions/GameActionException.java
+++ src/game/engine/exceptions/GameActionException.java
@@ -1,10 +1,8 @@
 package game.engine.exceptions;
 
 public abstract class GameActionException extends Exception {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	
+	/*private static final long serialVersionUID = 1L;*/
 	public GameActionException(){
 		super();
 	}
--- src/game/engine/interfaces/CanisterModifier.java
+++ src/game/engine/interfaces/CanisterModifier.java
@@ -1,6 +1,6 @@
 package game.engine.interfaces;
-
+import game.engine.Monster;
 public interface CanisterModifier {
-
+    public void modifyCanister(Monster monster);
 }
 
