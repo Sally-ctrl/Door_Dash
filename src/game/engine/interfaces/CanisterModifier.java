790f44d5ab6070d82da786eff70494a26a7becdb
Sarah Abdelkhalek <sallyahmed571@gmail.com>
2026-03-10 20:56:49 +0200

    update the interface error

--- src/game/engine/interfaces/CanisterModifier.java
+++ src/game/engine/interfaces/CanisterModifier.java
@@ -1,5 +1,5 @@
 package game.engine.interfaces;
-import game.engine.Monster;
+import game.engine.monsters.Monster;
 public interface CanisterModifier {
     public void modifyCanister(Monster monster);
 }
