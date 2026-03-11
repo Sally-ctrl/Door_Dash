60002d7ec9100931f4de99c922e8cd341b0cd858
Sarah Abdelkhalek <sallyahmed571@gmail.com>
2026-03-11 23:04:32 +0200

    final for milestone 1

--- src/game/engine/exceptions/GameActionException.java
+++ src/game/engine/exceptions/GameActionException.java
@@ -2,7 +2,6 @@ package game.engine.exceptions;
 
 public abstract class GameActionException extends Exception {
 	
-	/*private static final long serialVersionUID = 1L;*/
 	public GameActionException(){
 		super();
 	}
