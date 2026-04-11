08dba085990fc60434ec3b878a84537039daef42
Sarah Abdelkhalek <sarah0abdelkhalek@gmail.com>
2026-04-11 11:31:43 +0200

    energy in energy steal card and in schemer

--- src/game/engine/cards/EnergyStealCard.java
+++ src/game/engine/cards/EnergyStealCard.java
@@ -1,6 +1,7 @@
 package game.engine.cards;
 
 import game.engine.interfaces.CanisterModifier;
+import game.engine.monsters.Monster;
 
 public class EnergyStealCard extends Card implements CanisterModifier {
 	private int energy;
@@ -14,4 +15,32 @@ public class EnergyStealCard extends Card implements CanisterModifier {
 		return energy;
 	}
 	
+	public void performAction(Monster player, Monster opponent){
+		if(opponent.isShielded()){
+			opponent.setShielded(false);
+			return;
+		}
+
+		int amountToSteal = this.energy;
+
+		if(opponent.getEnergy()<amountToSteal){
+			amountToSteal=opponent.getEnergy();
+		}
+
+		if (amountToSteal <= 0) return;
+
+		opponent.alterEnergy(-amountToSteal);
+		player.alterEnergy(amountToSteal);
+		/*modifyCanisterEnergy(opponent, -100);
+			modifyCanisterEnergy(player, +100);
+ 			Bug:
+		opponent shield might block its own negative effect
+		BUT player still gains energy */
+		
+	}
+
+	@Override
+	public void modifyCanisterEnergy(Monster monster, int canisterValue) {
+    	monster.alterEnergy(canisterValue);
+	}
 }
--- src/game/engine/monsters/Monster.java
+++ src/game/engine/monsters/Monster.java
@@ -101,5 +101,7 @@ public abstract class Monster implements Comparable<Monster> {
 		
 	}
 
+	
+
 
 }
